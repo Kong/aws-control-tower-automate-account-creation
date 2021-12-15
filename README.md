84eaeb9fdee0ce3d544ea2c3674f8051af25050b
saisatishkarra <saisatish.karra@konghq.com>
2021-12-15 15:10:04 -0600

    Customize sync lambdas

--- README.md
+++ README.md
@@ -61,3 +61,6 @@ See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more inform
 
 This library is licensed under the MIT-0 License. See the LICENSE file.
 
+
+## Upload to Infra to S3 from root of the repository
+```aws-vault exec <profile> -- ./functions/sync_lambdas.sh```
--- functions/sync_lambdas.sh
+++ functions/sync_lambdas.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 echo
-echo "new_account_handler.py"
-echo "======================"
-pylint new_account_handler.py  |grep '^Your code has been rated'
-echo
-echo "account_create.py"
-echo "================="
-pylint account_create.py | grep '^Your code has been rated'
-echo
 echo "Packging the files"
 echo "======== === ====="
-zip -r ct_batchcreation_lambda.zip new_account_handler.py cfnresource.py 
-zip -r ct_account_create_lambda.zip account_create.py cfnresource.py
+cd functions/source
+zip -r ../../ct_batchcreation_lambda.zip new_account_handler.py cfnresource.py 
+zip -r ../../ct_account_create_lambda.zip account_create.py cfnresource.py
 echo
-for region in $(aws ec2 describe-regions --query 'Regions[*].RegionName' --output text)
-do
+region="us-east-1"
 echo "Copying to $region"
 echo "======= == ========="
-aws s3 cp ct_account_create_lambda.zip s3://marketplace-sa-resources-ct-${region}/ --acl public-read --profile d_mysc
-aws s3 cp ct_batchcreation_lambda.zip s3://marketplace-sa-resources-ct-${region}/ --acl public-read --profile d_mysc
+aws s3 cp ../../ct_account_create_lambda.zip s3://eng-enablement/
+aws s3 cp ../../ct_batchcreation_lambda.zip s3://eng-enablement/
+aws s3 cp ../../BatchAccountCreation.yaml s3://eng-enablement/BatchAccountCreation.yaml
 echo
-done
